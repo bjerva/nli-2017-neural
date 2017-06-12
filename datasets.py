6cf375485c01d58a506f0bc28239d9df47c6ba34
Johannes Bjerva <johannesbjerva@gmail.com>
2017-06-12 13:16:48 +0200

    Added

---
+++ datasets.py
@@ -0,0 +1,191 @@
+'''Preprocess IMDB dataset.'''
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+import sys
+import os
+import argparse
+import pickle
+import random
+
+import numpy as np
+
+from chainer.dataset import dataset_mixin
+from chainer.iterators import SerialIterator
+
+from collections import defaultdict
+
+
+def convert_file(filepath, char_to_id):
+    label = 0 if 'pos' in filepath else 1
+
+    with open(filepath) as ifile:
+        return [char_to_id[c] for w in ifile.read().split(' ') for c in w]
+
+def discover_dataset(path, char_to_id):
+    dataset = []
+    for root, _, files in os.walk(path):
+        for sfile in [f for f in files if '.txt' in f]:
+            filepath = os.path.join(root, sfile)
+            dataset.append(convert_file(filepath, char_to_id))
+    return dataset
+
+def find_ngrams(string, n):
+    return list(zip(*[string[i:] for i in range(n)]))
+
+def read_nli(data_dir, fold, char_to_id, label_to_id, maxlen=128):
+    unk = char_to_id['<UNK>']
+    bos = char_to_id['<S>']
+    eos = char_to_id['</S>']
+
+    train_label_path = os.path.join(data_dir, 'labels', fold, 'labels.{0}.csv'.format(fold))
+    labels = {}
+    with open(train_label_path, 'r') as in_f:
+        in_f.readline() # skip csv header
+        for line in in_f:
+            fields = line.strip().split(',')
+            entry_id, native_lang = fields[0], fields[3]
+            if native_lang not in ['FRE', 'JPN', 'TUR']: continue
+            labels[entry_id] = label_to_id[native_lang]
+
+    use_tokenized = False
+    version = 'tokenized' if use_tokenized else 'original'
+    train_essay_path = os.path.join(data_dir, 'essays', fold, version)
+    sents = defaultdict(list)
+    for root, dirs, files in os.walk(train_essay_path):
+        for idx, fname in enumerate(files):
+            if fname[:-4] not in labels: continue
+            with open(os.path.join(train_essay_path, fname)) as in_f:
+                #for line in in_f:
+                #char_rep = [[bos] + [char_to_id[char] for char in word] + [eos] for line in in_f for word in line.split()]
+                #for line in in_f:
+                #char_rep = [bos] + [char_to_id[ngram] for line in in_f for ngram in find_ngrams(line, 4)+find_ngrams(line, 3)+find_ngrams(line, 2)+find_ngrams(line, 1)] + [eos]
+                char_rep = [bos] + [char_to_id[ngram] for line in in_f for ngram in find_ngrams(line, 2)] + [eos]
+                # char_rep = []
+                # for line in in_f:
+                #     for n in range(6):
+                #         char_rep.extend([char_to_id[''.join(ngram)] for ngram in find_ngrams(line, n)])
+
+                if char_rep: # No empty lines
+                    sents[fname[:-4]].append(char_rep)
+
+
+            #if idx > 20:
+            #    break
+    #import pdb; pdb.set_trace()
+    print(len(char_to_id))
+    X, y = [], []
+    for key, entry in sorted(sents.items(), key=lambda x: -len(x[1])):
+        #if labels[key] not in [10, 8]: continue
+        for sent in entry:
+            X.append(sent)
+            y.append(labels[key])
+
+    return X, y
+
+
+def pad_dataset(dataset, maxlen):
+    return np.array(
+        [np.pad(r, (0, maxlen-len(r)), mode='constant') if len(r) < maxlen else np.array(r[:maxlen], dtype=np.int32)
+         for r in dataset], dtype=np.int32)
+
+#%%
+class IMDBDataset(dataset_mixin.DatasetMixin):
+    def __init__(self, path, dict_path, char_to_id, maxlen=128):
+        pos_path = os.path.join(path, 'pos')
+        neg_path = os.path.join(path, 'neg')
+
+        #with open(dict_path, 'rb') as dfile:
+        #    wdict = pickle.load(dfile)
+
+        self.pos_dataset = pad_dataset(discover_dataset(pos_path, char_to_id), maxlen).astype('i')
+        self.neg_dataset = pad_dataset(discover_dataset(neg_path, char_to_id), maxlen).astype('i')
+
+    def __len__(self):
+        return len(self.pos_dataset) + len(self.neg_dataset)
+
+    def get_example(self, i):
+        is_neg = i >= len(self.pos_dataset)
+        dataset = self.neg_dataset if is_neg else self.pos_dataset
+        idx = i - len(self.pos_dataset) if is_neg else i
+        label = 0 if is_neg else 1
+
+        return (dataset[idx], np.array(label, dtype=np.int32))
+
+class NLIDataset(SerialIterator):
+    def __init__(self, path, fold, char_to_id, label_to_id, maxlen=128, batch_size=1, repeat=True, shuffle=True):
+        X, y = read_nli(path, fold, char_to_id, label_to_id, maxlen=maxlen)
+        print('{0} instances in {1}'.format(len(X), fold))
+        print('longest sentence is {0} chars'.format(max(map(len, X))))
+        self.dataset = list(zip(X, y))#pad_dataset(X, maxlen)
+        self.maxlen = maxlen
+        super(NLIDataset, self).__init__(
+            self.dataset, batch_size, repeat, shuffle)
+
+    def __next__(self):
+        if not self._repeat and self.epoch > 0:
+            raise StopIteration
+
+        self._previous_epoch_detail = self.epoch_detail
+
+        i = self.current_position
+        i_end = i + self.batch_size
+        N = len(self.dataset)
+
+        if self._order is None:
+            batch = self.get_data(self.dataset[i:i_end])
+        else:
+            batch = self.get_data([self.dataset[index] for index in self._order[i:i_end]])
+
+        if i_end >= N:
+            if self._repeat:
+                rest = i_end - N
+                if self._order is not None:
+                    np.random.shuffle(self._order)
+                if rest > 0:
+                    if self._order is None:
+                        batch.extend(self.get_data(self.dataset[:rest]))
+                    else:
+                        batch.extend(self.get_data([self.dataset[index]
+                                      for index in self._order[:rest]]))
+                self.current_position = rest
+            else:
+                self.current_position = 0
+
+            self.epoch += 1
+            self.is_new_epoch = True
+        else:
+            self.is_new_epoch = False
+            self.current_position = i_end
+
+        return batch
+
+    next = __next__ # This appears to be necessary for some reason...
+
+    def get_data(self, data):
+        X, y = zip(*data)
+
+        mlen = self.maxlen#max(map(len, X))
+        #mlen_word = 16
+        # if max(map(len, X)) > mlen:
+        #     print(max(map(len, X)))
+        X = np.asarray([sent[:mlen] + [-1]*(mlen-len(sent)) for sent in X], dtype=np.int32)
+        # X_onehot = np.zeros((self.batch_size, 300000), dtype=np.float32)
+        # for idx, sent in enumerate(X):
+        #     for word_id in sent:
+        #         X_onehot[idx, word_id] += 1
+        #
+        # X = X_onehot
+        # X = [[word[:mlen_word] + [-1]*(mlen_word-len(word)) for word in sent] for sent in X]
+        # X = [sent[:mlen] + [[-1]*mlen_word for _ in range(mlen-len(sent))] for sent in X]
+        # X = np.asarray(X, dtype=np.int32)
+        #import pdb; pdb.set_trace()
+        return list(zip(X, np.array(y, dtype=np.int32)))#.reshape(-1, 1)))#list(zip(X_l1_words, X_l2_indices, X_l2_contexts, y))
+
+#%%
+# import chainer
+# train = IMDBDataset('data/aclImdb/train', 'data/dict.pckl')
+# train_iter = chainer.iterators.SerialIterator(train, 16)
+
+
+# batch = next(train_iter)
+# batch[0]
---
+++ train.py
@@ -0,0 +1,396 @@
+from __future__ import print_function
+import argparse
+import os
+
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import training
+from chainer.training import extensions
+
+#from models.qrnn import QRNNLayer
+from datasets import IMDBDataset, NLIDataset
+#import util#.util as util
+
+from collections import defaultdict
+
+
+class QRNNModel(chainer.Chain):
+    def __init__(self, vocab_size, n_labels, out_size, hidden_size, dropout):
+        super().__init__(
+            embed = L.EmbedID(vocab_size, out_size, ignore_label=-1),
+            char_lstm=L.LSTM(out_size, hidden_size//2),
+            layer1=QRNNLayer(hidden_size//2, hidden_size, kernel_size=2),
+            layer2=QRNNLayer(hidden_size, hidden_size, kernel_size=2),
+            layer3=QRNNLayer(hidden_size, hidden_size),
+            layer4=QRNNLayer(hidden_size, hidden_size),
+            fc=L.Linear(None, n_labels)
+        )
+        self.dropout = dropout
+        self.train = True
+
+    def __call__(self, x):
+        hs = []
+        for sent in x:
+            word_reps = []
+            for word in sent:
+                h = self.embed(word)
+                self.char_lstm.reset_state()
+                h = F.dropout(self.char_lstm(h), self.dropout, self.train)
+                word_reps.append(h[-1])
+
+            hs.append(F.vstack(word_reps))
+
+        #import pdb; pdb.set_trace()
+        h = F.vstack(hs).reshape(x.shape[0], -1, 64)#.shape#F.hstack(hs)
+        #import pdb; pdb.set_trace()
+        h = F.dropout(self.layer1(h), self.dropout, self.train)
+        h = F.dropout(self.layer2(h), self.dropout, self.train)
+        h = F.dropout(self.layer3(h), self.dropout, self.train)
+        h = F.dropout(self.layer4(h), self.dropout, self.train)
+        return self.fc(F.dropout(h, self.dropout, self.train))
+
+
+class RNNModel(chainer.Chain):
+    def __init__(self, vocab_size, n_labels, out_size, hidden_size, dropout):
+        super().__init__(
+            embed=L.EmbedID(vocab_size, out_size, ignore_label=-1),
+            char_lstm=L.LSTM(out_size, hidden_size//2),
+            word_lstm=L.LSTM(hidden_size//2, hidden_size),
+            fc=L.Linear(None, n_labels)
+        )
+        self.dropout = dropout
+        self.train = True
+
+    def reset_state(self):
+        self.char_lstm.reset_state()
+        self.word_lstm.reset_state()
+
+    def __call__(self, x):
+        # hs = []
+        # for sent in x:
+        #     self.word_lstm.reset_state()
+        #     word_reps = []
+        #     for word in sent:
+        #         self.char_lstm.reset_state()
+        #         h = F.dropout(self.embed(word), self.dropout, self.train)
+        #         h = F.dropout(self.char_lstm(h), self.dropout, self.train)
+        #         word_reps.append(h)
+        #
+        #     sent_rep = F.vstack(word_reps)
+        #     h = F.dropout(self.word_lstm(sent_rep), self.dropout, self.train)
+        #     hs.append(h[-1])
+        #
+        # return self.fc(F.vstack(hs))
+        h = self.embed(x)
+        #import pdb; pdb.set_trace()
+        self.char_lstm.reset_state()
+        for idx, seq in enumerate(F.transpose_sequence(h)):
+            h = self.char_lstm(seq)
+
+        #print('b')
+
+        return self.fc(h)
+
+class CNNModel(chainer.Chain):
+    def __init__(self, vocab_size, n_labels, out_size, hidden_size, dropout):
+        output_channel = hidden_size
+        sent_len = args.maxlen
+        out_channels = int(args.maxlen*2)
+        super().__init__(
+            embed=L.EmbedID(vocab_size, out_size, ignore_label=-1),
+            # conv1 = L.Convolution2D(sent_len, out_channels, (2, 2)),
+            # conv2 = L.Convolution2D(out_channels, out_channels*2, (2, 2)),
+            # l1    = L.Linear(480, hidden_size),
+            # fc=L.Linear(None, n_labels)
+
+            bn0 = L.BatchNormalization(out_size),
+            conv1 = L.ConvolutionND(ndim=1,
+                in_channels=out_size, out_channels=out_size*2, ksize=4, stride=4, cover_all=True),
+            bn1 = L.BatchNormalization(out_size*2),
+            conv2 = L.ConvolutionND(ndim=1,
+                in_channels=out_size*2, out_channels=1024, ksize=4, stride=4, cover_all=True),
+            bn2 = L.BatchNormalization(1024),
+            conv3 = L.ConvolutionND(ndim=1,
+                in_channels=out_size*3, out_channels=out_size*4, ksize=4, stride=2),
+            conv4 = L.ConvolutionND(ndim=1,
+                in_channels=out_size*4, out_channels=out_size*5, ksize=4, stride=2),
+            conv5 = L.ConvolutionND(ndim=1,
+                in_channels=out_size*5, out_channels=out_size*6, ksize=4, stride=2),
+            #rnn1 = L.LSTM(None, 256),
+            #fc4 = L.Linear(None, 1024),
+            fc5 = L.Linear(None, 512),
+            fc6 = L.Linear(512, 128),
+            fc7 = L.Linear(128, n_labels)
+        )
+        self.dropout = dropout
+        self.train = True
+        self.first = True
+
+    def __call__(self, x):
+        #self.rnn1.reset_state()
+
+        h = self.embed(x)
+        if self.first:
+            print('emb', h.data.shape)
+
+
+
+        h = F.swapaxes(h, 1, 2)
+        prev_h = h
+
+
+        # Block 1
+        if args.bn:
+            h = self.bn0(h)
+        if args.activation:
+            h = F.relu(h)
+        h = F.dropout(h, self.dropout)
+        if self.first:
+            print('swp', h.data.shape)
+        h = self.conv1(h)
+        if self.first:
+            print('cv1', h.data.shape)
+
+        # Block 2
+        if args.bn:
+            h = self.bn1(h)
+        if args.activation:
+            h = F.relu(h)
+        h = F.dropout(h, self.dropout)
+        h = self.conv2(h)
+        if self.first:
+            print('cv2', h.data.shape)
+
+
+        h = F.average_pooling_nd(h, 2)
+        if self.first:
+            print('av1', h.data.shape)
+
+        # prev_h = F.average_pooling_nd(prev_h, 4)
+        # prev_h = F.swapaxes(prev_h, 1, 2)
+        # if self.first:
+        #     print('av2', prev_h.data.shape)
+        # h = F.concat((prev_h, h)) # Residual yo
+        # if self.first:
+        #     print('mc1', h.data.shape)
+
+        # h = F.average_pooling_nd(F.dropout(h, self.dropout), 4)
+        # if self.first:
+        #     print('av2', h.data.shape)
+        # #
+        # # if self.first:
+        # #     print(h.data.shape)
+        # h = self.conv4(h)
+        # if self.first:
+        #     print(h.data.shape)
+        # h = self.conv5(h)
+        # if self.first:
+        #     print(h.data.shape)
+        # h = F.swapaxes(h, 1, 2)
+        # h = F.average_pooling_nd(h, 2)
+        # h = F.swapaxes(h, 1, 2)
+        # if self.first:
+        #     print(h.data.shape)
+
+        #h = self.rnn1(h)
+        # # h = F.max_pooling_nd(h, 4, cover_all=True)
+        # if self.first:
+        #     print('after lstm')
+        #     print(h.data.shape)
+        # h = self.fc4(F.dropout(h, self.dropout, self.train))
+        # if self.first:
+            # print(h.data.shape)
+
+        if self.first:
+            print('Hidden has to deal with {0} units as input'.format(h.shape[1]*h.shape[2]))
+        h = F.relu(h)
+        h = F.dropout(h, self.dropout)
+        h = self.fc5(h)
+        if self.first:
+            print('fc5', h.data.shape)
+
+        h = F.relu(h)
+        h = F.dropout(h, self.dropout)
+        h = self.fc6(h)
+        if self.first:
+           print('fc6', h.data.shape)
+
+        h = F.relu(h)
+        if self.first:
+            #import pdb; pdb.set_trace()
+            self.first = False
+        #import pdb; pdb.set_trace()
+        #h = self.l1(F.dropout(F.average_pooling_2d(h, 2), self.dropout, self.train))
+        return self.fc7(F.dropout(h, self.dropout))
+
+        # import pdb; pdb.set_trace()
+        # #h = F.vstack(hs).reshape(x.shape[0], -1, 64)#.shape#F.hstack(hs)
+        # #import pdb; pdb.set_trace()
+        # h = F.dropout(self.layer1(h), self.dropout, self.train)
+        # h = F.dropout(self.layer2(h), self.dropout, self.train)
+        # h = F.dropout(self.layer3(h), self.dropout, self.train)
+        # h = F.dropout(self.layer4(h), self.dropout, self.train)
+        # return self.fc(F.dropout(h, self.dropout, self.train))
+
+class FFNNModel(chainer.Chain):
+    def __init__(self, vocab_size, n_labels, hidden_size, dropout):
+        super().__init__(
+            fc0 = L.Linear(None, 2048),
+            fc1 = L.Linear(None, 512),
+            fc2 = L.Linear(512, 128),
+            fc3 = L.Linear(128, n_labels)
+        )
+        self.dropout = dropout
+        self.train = True
+        self.first = True
+
+    def __call__(self, x):
+        h = self.fc0(x)
+        h = self.fc1(F.dropout(h, self.dropout, self.train))
+        h = self.fc2(F.dropout(h, self.dropout, self.train))
+        #h = self.fc2(F.dropout(h, self.dropout, self.train))
+        h = self.fc3(F.dropout(h, self.dropout, self.train))
+        return h
+        # h = F.relu(h)
+        # h = self.fc5(F.dropout(h, self.dropout, self.train))
+        # if self.first:
+        #     print('fc5', h.data.shape)
+        #
+        # h = F.relu(h)
+        # h = self.fc6(F.dropout(h, self.dropout, self.train))
+        # if self.first:
+        #    print('fc6', h.data.shape)
+        #
+        # h = F.relu(h)
+        # if self.first:
+        #     #import pdb; pdb.set_trace()
+        #     self.first = False
+        # return self.fc7(F.dropout(h, self.dropout, self.train))
+# epoch       main/loss   validation/main/loss  main/accuracy  validation/main/accuracy
+# 1           1.26909     0.827249              0.722          0.683333
+# 2           0.628819    0.570169              0.835          0.82
+# 3           0.304336    0.545898              0.903667       0.866667
+# 4           0.382191    1.22191               0.902          0.726667
+# 5           0.205926    0.700033              0.935667       0.843333
+
+class TestModeEvaluator(extensions.Evaluator):
+
+    def evaluate(self):
+        model = self.get_target('main')
+        model.train = False
+        ret = super().evaluate()
+        model.train = True
+        return ret
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', '-b', type=int, default=24,
+                        help='Number of documents in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=0,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='',
+                        help='Resume the training from snapshot')
+    parser.add_argument('--embeddings', default='',
+                        help='Iinitial word embeddings file')
+    parser.add_argument('--vocabulary', default='',
+                        help='Vocabulary file')
+    parser.add_argument('--dataset', default='data/aclImdb', type=str,
+                        help='IMDB dataset path, dir with train/ and test/ folders')
+    parser.add_argument('--vocab_size', default=68379, type=int,
+                        help='GloVe word embedding dimensions')
+    parser.add_argument('--out_size', default=300, type=int,
+                        help='GloVe word embedding dimensions')
+    parser.add_argument('--hidden_size', default=256, type=int,
+                        help='Hidden layers dimensions')
+    parser.add_argument('--maxlen', default=400, type=int,
+                        help='Maximum sequence time (T) length')
+    parser.add_argument('--dropout', default=0.3, type=float,
+                        help='Dropout ratio between layers')
+    parser.add_argument('--activation', action='store_true')
+    parser.add_argument('--bn', action='store_true')
+    return parser.parse_args()
+
+
+args = parse_args()
+def main():
+
+
+    char_to_id = defaultdict(lambda: len(char_to_id))
+    label_to_id = defaultdict(lambda: len(label_to_id))
+    unk = char_to_id['<UNK>']
+    bos = char_to_id['<S>']
+    eos = char_to_id['</S>']
+    if 'Imdb' in args.dataset:
+        train, test = IMDBDataset(os.path.join(args.dataset, 'train'), args.vocabulary, char_to_id, args.maxlen),\
+                      IMDBDataset(os.path.join(args.dataset, 'test'), args.vocabulary, char_to_id, args.maxlen)
+
+    elif 'nli' in args.dataset:
+        print('nli!')
+        train = NLIDataset(args.dataset, 'train', char_to_id, label_to_id, args.maxlen, args.batchsize, repeat=True)
+        test = NLIDataset(args.dataset, 'dev', char_to_id, label_to_id, args.maxlen, args.batchsize, repeat=False, shuffle=False)
+
+    vocab_size = len(char_to_id) + 1#max(map(max, train.pos_dataset))
+    n_labels = 2 if len(label_to_id) == 0 else len(label_to_id)
+
+    print('{0} chars'.format(vocab_size))
+    print('{0} labels'.format(n_labels))
+    # model = L.Classifier(QRNNModel(
+    #    vocab_size, n_labels, args.out_size, args.hidden_size, args.dropout))
+
+    # model = L.Classifier(RNNModel(
+    #    vocab_size, n_labels, args.out_size, args.hidden_size, args.dropout))
+
+    model = L.Classifier(CNNModel(
+        vocab_size, n_labels, args.out_size, args.hidden_size, args.dropout))
+
+    # model = L.Classifier(FFNNModel(
+    #     vocab_size, n_labels,args.hidden_size, args.dropout))
+
+    if args.embeddings:
+        model.predictor.embed.W.data = util.load_embeddings(
+            args.embeddings, args.vocab_size, args.out_size)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.to_gpu()
+        model.predictor.embed.to_gpu()
+        # model.predictor.char_lstm.to_gpu()
+
+    optimizer = chainer.optimizers.Adam()#RMSprop(lr=0.001, alpha=0.9)
+    optimizer.use_cleargrads()
+    optimizer.setup(model)
+    optimizer.add_hook(chainer.optimizer.WeightDecay(1e-4))
+
+    if 'Imdb' in args.dataset:
+        train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+        test_iter = chainer.iterators.SerialIterator(test, 1,
+                                                     repeat=False, shuffle=False)
+    elif 'nli' in args.dataset:
+        train_iter = train
+        test_iter = test
+
+    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+
+    trainer.extend(TestModeEvaluator(test_iter, model, device=args.gpu))
+    #trainer.extend(extensions.ExponentialShift('lr', 0.5), trigger=(25, 'epoch'))
+    trainer.extend(extensions.snapshot(), trigger=(args.epoch, 'epoch'))
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss', 'validation/main/loss',
+         'main/accuracy', 'validation/main/accuracy']))
+    trainer.extend(extensions.ProgressBar(update_interval=10))
+
+    if args.resume:
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    trainer.run()
+
+if __name__ == '__main__':
+    main()
